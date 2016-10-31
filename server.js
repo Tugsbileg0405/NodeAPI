49e25d6f4979165613884beb6df89fdf34fbb349
Tugsbileg <tugu0405@gmail.com>
2016-10-31 18:57:39 +0800

    ''

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-
+var cors = require('cors');
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User = require('./app/models/user'); // get our mongoose model
@@ -21,7 +21,7 @@ app.set('superSecret', config.secret); // secret variable
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
